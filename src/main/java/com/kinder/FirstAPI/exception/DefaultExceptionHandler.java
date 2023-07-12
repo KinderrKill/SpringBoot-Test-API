2f1fffc03946858dbed1468d014c678f118eded7
Étienne S <sauvage-etienne@hotmail.com>
2023-07-12 16:20:52 +0200

    Fix: DefaultExceptionHandler
    Forget to add @ControllerAdvice

--- src/main/java/com/kinder/FirstAPI/exception/DefaultExceptionHandler.java
+++ src/main/java/com/kinder/FirstAPI/exception/DefaultExceptionHandler.java
@@ -3,13 +3,15 @@ package com.kinder.FirstAPI.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
+@ControllerAdvice
 public class DefaultExceptionHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
+    @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiError> handleException(Exception e, HttpServletRequest request) {
         ApiError apiError = new ApiError(
                 request.getRequestURI(),
